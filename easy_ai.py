d0b2aefdfe83184c118c06accb8ff3d6adfa1c47
mattfirth7 <mmf001x@yahoo.com>
2020-04-25 21:27:49 -0400

    Version 0.0.2 main method finished but data reshaping is not working

--- easy_ai.py
+++ easy_ai.py
@@ -141,12 +141,19 @@ def quadratic_discriminant(x_train, y_train, x_test, y_test):
 def easy_classification(x_train, y_train, x_test, y_test):
     results_array = []
     results_array.append(k_nearest(x_train, y_train, x_test, y_test))
+    print(k_nearest(x_train, y_train, x_test, y_test)[1])
     results_array.append(support_vector_machine(x_train, y_train, x_test, y_test))
+    print(support_vector_machine(x_train, y_train, x_test, y_test)[1])
     results_array.append(decision_tree_classifier(x_train, y_train, x_test, y_test))
+    print(decision_tree_classifier(x_train, y_train, x_test, y_test)[1])
     results_array.append(random_forest_classifier(x_train, y_train, x_test, y_test))
+    print(random_forest_classifier(x_train, y_train, x_test, y_test)[1])
     results_array.append(adaboost_classifier(x_train, y_train, x_test, y_test))
+    print(adaboost_classifier(x_train, y_train, x_test, y_test)[1])
     results_array.append(native_bayes(x_train, y_train, x_test, y_test))
-    results_array.append(quadratic_discriminant(x_train, y_train, x_test, y_test))
+    print(native_bayes(x_train, y_train, x_test, y_test)[1])
+    #quadratic discriminant analysis is not currently working
+    #results_array.append(quadratic_discriminant(x_train, y_train, x_test, y_test))
     
     accuracies_arr = []
     for row in results_array:
@@ -160,7 +167,8 @@ def easy_classification(x_train, y_train, x_test, y_test):
         f.write(model)
         f.close()
     
-    print(classifier + " model chosen. Yielded max accuracy: " + results_array[accuracy_argmax][1])
+    print(classifier + " model chosen. Yielded max accuracy: " + str(results_array[accuracy_argmax][1]))
+    print("Model saved as 'easyml_classifier.pickle'")
     return
     
 #main method still work in progress
@@ -171,12 +179,23 @@ def main():
     input_array = np.genfromtxt(input_filename, delimiter = ',', skip_header = 1)
     y = input_array[-1]
     x = np.delete(input_array,-1,0)
-    
+    print(x[:5])
     sklearn_compatible_array = convert_to_sklearn_shape(x)
+    print(sklearn_compatible_array[:5])
     x_train, x_test, y_train, y_test = train_test_split(sklearn_compatible_array,y,test_size=0.2)
+    print(x_train[:5])
+    analysis_shape_x_train = convert_to_analysis_shape(x_train)
+    analysis_shape_normed_x_train = data_norm(analysis_shape_x_train)
+    print(analysis_shape_normed_x_train[:5])
+    analysis_shape_x_test = convert_to_analysis_shape(x_test)
+    analysis_shape_normed_x_test = data_norm(analysis_shape_x_test)
+    print(analysis_shape_normed_x_test[:5])
+    
+    sk_learn_compatible_normed_x_train = convert_to_sklearn_shape(analysis_shape_normed_x_train)
+    sk_learn_compatible_normed_x_test = convert_to_sklearn_shape(analysis_shape_normed_x_test)
     
     
-    #easy_classification
+    easy_classification(sk_learn_compatible_normed_x_train, y_train, sk_learn_compatible_normed_x_test, y_test)
     
 if __name__ == '__main__':
     main()
